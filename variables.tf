62b6b412c216755d4d8ae889db713dd666dc9f20
SakshiSawant419 <sawant.sak@northeastern.edu>
2025-02-19 01:10:25 -0500

    Terraform

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_profile" {
+  description = "AWS profile to use"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
