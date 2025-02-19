a307895060ddb0e963383fb35164c395255f5ebf
SakshiSawant419 <sawant.sak@northeastern.edu>
2025-02-19 16:52:06 -0500

    Assignment 3 Terraform

--- route_tables.tf
+++ route_tables.tf
@@ -1,13 +1,36 @@
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "public-route-table"
+    Name = "PublicRouteTable"
   }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
--- variables.tf
+++ variables.tf
@@ -1,15 +1,30 @@
-variable "aws_profile" {
-  description = "AWS profile to use"
-  type        = string
-}
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "aws_profile" {
+  description = "AWS profile name"
   type        = string
-  default     = "10.0.0.0/16"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
